2d3e03d56e300ae42bc0b477655caa169e622bc8
6187144 <6187144@cegep-heritage.qc.ca>
2022-03-29 14:15:02 -0400

    lab 3

--- Model/User.cs
+++ Model/User.cs
@@ -5,7 +5,9 @@ namespace w21_lab3.Models;
 public class User
 {
     public int UserId { get; set; }
+    [MinLength(3, ErrorMessage = "Too short name"), MaxLength(40, ErrorMessage = "Name limit 40 characters")]
     public string? Name { get; set; }
+    [Required]
     public string? Email { get; set; }
     public int? StreetNumber { get; set; }
     public string? StreetName { get; set; }
@@ -13,6 +15,7 @@ public class User
     public string? PostalCode { get; set; }
     [Required]
     public string? City { get; set; }
+    [MinLength(2, ErrorMessage = "Please use 2 letter abb. for the province"), MaxLength(2, ErrorMessage = "Please use 2 letter abb. for the province")]
     public string? Province { get; set; }
     public string? Phone { get; set; }
 
